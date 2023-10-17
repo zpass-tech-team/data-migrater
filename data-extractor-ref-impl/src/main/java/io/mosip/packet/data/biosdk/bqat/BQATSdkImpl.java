80bfd52de87e151c17aff5720f4bc632287f80f6
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2023-10-17 07:15:23 +0530

    Modification on Migration Utility

--- data-extractor-ref-impl/src/main/java/io/mosip/packet/data/biosdk/bqat/BQATSdkImpl.java
+++ data-extractor-ref-impl/src/main/java/io/mosip/packet/data/biosdk/bqat/BQATSdkImpl.java
@@ -26,6 +26,8 @@ import java.util.concurrent.TimeUnit;
 
 import static io.mosip.packet.core.constant.GlobalConfig.IS_ONLY_FOR_QUALITY_CHECK;
 import static io.mosip.packet.core.constant.GlobalConfig.WRITE_BIOSDK_RESPONSE;
+import static io.mosip.packet.core.constant.RegistrationConstants.APPLICATION_ID;
+import static io.mosip.packet.core.constant.RegistrationConstants.APPLICATION_NAME;
 
 @Component
 public class BQATSdkImpl implements BioSdkApiFactory {
@@ -38,17 +40,20 @@ public class BQATSdkImpl implements BioSdkApiFactory {
 
     @Override
     public Double calculateBioQuality(BioSDKRequestWrapper bioSDKRequestWrapper) throws Exception {
-        BQATRequest request = new BQATRequest();
         Long startTime = System.nanoTime();
+        LOGGER.debug("SESSION_ID", APPLICATION_NAME, APPLICATION_ID, "Entering BIOSDK for Quality Calculation" + TimeUnit.SECONDS.convert(System.nanoTime()-startTime, TimeUnit.NANOSECONDS));
+
+        BQATRequest request = new BQATRequest();
         request.setModality(BQATModalityType.valueOf(bioSDKRequestWrapper.getBiometricType()).getModality());
         request.setType(BQATFileType.valueOf(bioSDKRequestWrapper.getFormat()).getType());
         request.setData(Base64.getEncoder().encodeToString(((BIR)bioSDKRequestWrapper.getSegments().get(0)).getBdb()));
         request.setId(UUID.randomUUID().toString());
         request.setTimestamp(LocalDateTime.now(ZoneId.of("UTC")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")));
+        LOGGER.debug("SESSION_ID", APPLICATION_NAME, APPLICATION_ID, "Request Preparation Completed " + TimeUnit.SECONDS.convert(System.nanoTime()-startTime, TimeUnit.NANOSECONDS));
+
         BQATResponse response= (BQATResponse) restApiClient.postApi(ApiName.BQAT_BIOSDK_QUALITY_CHECK, null, "", request, BQATResponse.class);
         LinkedHashMap<String, Object> bioSDKResponse = (LinkedHashMap<String, Object>) response.getResults();
-        Long time = System.nanoTime() - startTime;
-        System.out.println("Time Taken to call BIOSDK is" + TimeUnit.MINUTES.convert(time, TimeUnit.NANOSECONDS));
+        LOGGER.debug("SESSION_ID", APPLICATION_NAME, APPLICATION_ID, "Rest Call Executed Successfully " + TimeUnit.SECONDS.convert(System.nanoTime()-startTime, TimeUnit.NANOSECONDS));
         if(bioSDKResponse != null) {
             try {
                 if(bioSDKRequestWrapper.getIsOnlyForQualityCheck()) {
@@ -72,10 +77,14 @@ public class BQATSdkImpl implements BioSdkApiFactory {
                 else
                     return Double.valueOf(0);
             } finally {
+                LOGGER.debug("SESSION_ID", APPLICATION_NAME, APPLICATION_ID, "Existing BIOSDK for Quality Calculation" + TimeUnit.SECONDS.convert(System.nanoTime()-startTime, TimeUnit.NANOSECONDS));
+
                 if(WRITE_BIOSDK_RESPONSE) {
                     HashMap<String, String> csvMap = (HashMap<String, String>) bioSDKRequestWrapper.getInputObject();
                     csvMap.put(bioSDKRequestWrapper.getBiometricField(),  (new Gson()).toJson(bioSDKResponse));
                 }
+                LOGGER.debug("SESSION_ID", APPLICATION_NAME, APPLICATION_ID, "Writing Response into CSVMAP" + TimeUnit.SECONDS.convert(System.nanoTime()-startTime, TimeUnit.NANOSECONDS));
+
             }
         } else {
             LOGGER.error("Error While Calling BIOSDK for Quality Check for Modality " + (new Gson()).toJson(response));
