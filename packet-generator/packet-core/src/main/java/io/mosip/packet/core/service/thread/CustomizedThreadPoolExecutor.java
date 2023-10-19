924d619f9d97853f408f61513d6ae627d472d543
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2023-10-19 10:52:11 +0530

    Modification on Migration Utility

--- packet-generator/packet-core/src/main/java/io/mosip/packet/core/service/thread/CustomizedThreadPoolExecutor.java
+++ packet-generator/packet-core/src/main/java/io/mosip/packet/core/service/thread/CustomizedThreadPoolExecutor.java
@@ -20,6 +20,7 @@ public class CustomizedThreadPoolExecutor {
     private Timer watch = null;
     private Timer estimateTimer = null;
     private String NAME;
+    private FixedListQueue<Long> timeConsumptionPerMin = new FixedListQueue<>(100);
     private FixedListQueue<Integer> countOfProcessPerMin = new FixedListQueue<>(100);
 
     public CustomizedThreadPoolExecutor(Integer threadPoolCount, Integer maxThreadCount, Integer maxThreadExecCount, String poolName) {
@@ -39,7 +40,10 @@ public class CustomizedThreadPoolExecutor {
 
                     Long avgTime = 0l;
                     Long[] consumedTimeList = listQueue.toArray(new Long[listQueue.size()]);
+                    Long TotalSum = Arrays.stream(consumedTimeList).mapToLong(Long::longValue).sum();
                     int noOfRecords = consumedTimeList.length;
+                    avgTime = TotalSum / noOfRecords;
+                    timeConsumptionPerMin.add(avgTime);
                     countOfProcessPerMin.add(noOfRecords);
                 }
             }
@@ -107,10 +111,14 @@ public class CustomizedThreadPoolExecutor {
 
                 if(totalTaskCount > 0 || totalCount > 0) {
                     // Calculating Estimated Time of Process Completion
-                    if(countOfProcessPerMin != null && countOfProcessPerMin.size() > 0) {
+                    if(timeConsumptionPerMin != null && timeConsumptionPerMin.size() > 0) {
+                        FixedListQueue<Long> listQueue = (FixedListQueue<Long>) timeConsumptionPerMin.clone();
                         FixedListQueue<Integer> countQueue = (FixedListQueue<Integer>)countOfProcessPerMin.clone();
 
+                        Long[] consumedTimeList = listQueue.toArray(new Long[listQueue.size()]);
                         Long totalRecords = TOTAL_RECORDS_FOR_PROCESS;
+                        Long TotalSum = Arrays.stream(consumedTimeList).mapToLong(Long::longValue).sum();
+                        int noOfRecords = consumedTimeList.length;
 
                         Integer[] consumedCountList = countQueue.toArray(new Integer[countQueue.size()]);
                         Integer TotalCountSum = Arrays.stream(consumedCountList).mapToInt(Integer::intValue).sum();
@@ -118,7 +126,11 @@ public class CustomizedThreadPoolExecutor {
                         int avgCount = TotalCountSum/noOfCountRecords;
 
                         Long remainingRecords = totalRecords - (totalCompletedTaskCount+ completedCount + ALREADY_PROCESSED_RECORDS);
+                        avgTime = TotalSum / noOfRecords;
                         Long totalTimeRequired = (remainingRecords / avgCount);
+                        System.out.println("remainingRecords" + remainingRecords);
+                        System.out.println("avgCount" + avgCount);
+                        System.out.println("Total Time Required" + totalTimeRequired);
 
                         totalHours = (int) (totalTimeRequired / 60);
                         totalDays = (int) totalHours / 24;
